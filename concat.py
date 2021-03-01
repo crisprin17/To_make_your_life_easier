da23e0d646f3bbc13a916d2d26aa8431136b1f04
Cristiana Principato <cristiana.principato@smiths.com>
2021-03-01 11:40:03 -0800

    fixing path

--- concat.py
+++ concat.py
@@ -3,15 +3,16 @@
 def print_usage():
     print "Usage: run_file.py"
 
+filepath ="path"
+filename = open("file_name","r+")
 
-filename = open("/nova/app/users/crisprin/GENIE_relS17_10_30/AllFlavor_newROCKBOX.txt","r+")
 print "----------------------1---------: "
 flines = filename.readlines()
 count = len(flines)
 print "nova -c concat_files.fcl"
 for j in range(1,len(flines)):
     try:
-        print(" `pnfs2xrootd /pnfs/nova/users/crisprin/GENIE/tautau/AllFlavor/"+str(flines[j]).strip()+"`")
+        print(" `pnfs2xrootd "+filepath+" "+str(flines[j]).strip()+"`")
     except ValueError:
         break
        
