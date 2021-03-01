38425dcaf92396d1bef8f6909e0049cb1453d7f4
Cristiana Principato <cristiana.principato@smiths.com>
2021-03-01 11:50:01 -0800

    test removing path

--- check_exit_status.py
+++ check_exit_status.py
@@ -6,12 +6,17 @@ import re
 def print_usage():
     print "Usage: check_exit_status.py"
 
+
+filepath ="path/"
+filename_er = "file_name"
+filename_out = "file_name"
+filename_original = "file_name"
 #when you fetch the output of the jobs you sent on the grid you will have a long list of out and err file that will tell 
 #you the results of your job. if they run ok the exit status will be 0.                                                  
-outfilenameERR = open("/nova/app/users/crisprin/python/filewithExitStatusNON_zero_err.txt","wb")
-outfilenameOUT = open("/nova/app/users/crisprin/python/filewithExitStatusNON_zero_out.txt","wb")
+outfilenameERR = open(filepath+filename_er,"wb")
+outfilenameOUT = open(filepath+filename_out,"wb")
 #files containin information about the exit status from two kind of files .err and .out
-with open("/nova/app/users/crisprin/Eval_S17-10-30/fetch/exit_status_ddupmu_bkg.txt", "r+") as f1:
+with open(filepath+filename_original, "r+") as f1:
     for name in f1:
         #take only the value of the exit status
         status = name.split(' ')[-1]
